23b9d784a828ea1b7124d50898d6a6419ff6a32f
Matthew Herod <matthew.herod@myunidays.com>
2017-09-28 09:12:12 +0100

    Some comments for fixes and temp fix

--- adb-kotlin/src/main/kotlin/co/herod/adbwrapper/testing/failOnText.kt
+++ adb-kotlin/src/main/kotlin/co/herod/adbwrapper/testing/failOnText.kt
@@ -13,9 +13,17 @@ fun AdbDeviceTestHelper.failOnText(
         timeout: Int = 20,
         timeUnit: TimeUnit = TimeUnit.SECONDS
 ) = with(adbDevice) {
+    // Required cases:
+    // fail - if text is visible throughout
+    // fail - if text becomes visible
+    // pass immediately - if text disappears before end of timeout
+    // fail fast mode - fail immediately - if text is visible within the first second
+    // pass fast mode - pass immediately - if text is not visible within the first second
     val deadlineMillis = timeUnit.toMillis(timeout.toLong())
     val timeMillis = measureTimeMillis {
-        Observable.timer(5, TimeUnit.MILLISECONDS)
+        // TEMP FIX to allow time for UI to refresh
+        Observable.timer(2000, TimeUnit.MILLISECONDS)
+        // Observable.timer(100, TimeUnit.MILLISECONDS)
                 .flatMap { uiNodeSource() }
                 .timeout(timeout, timeUnit)
                 .onErrorResumeNext { _: Throwable -> Observable.empty() }
@@ -25,7 +33,9 @@ fun AdbDeviceTestHelper.failOnText(
                     }
                 }
     }
-    if ((deadlineMillis * 1.1) < timeMillis) {
+    if ((deadlineMillis * 1.5) < timeMillis) {
         throw AssertionError("Exceeded provided timeout (took $timeMillis, expected $deadlineMillis)")
     }
+
+    // TODO this needs to assess the uiHierarchy completely
 }
