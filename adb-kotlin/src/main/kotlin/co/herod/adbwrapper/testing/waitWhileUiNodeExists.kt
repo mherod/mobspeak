c0ef646c6102a606ae99bc065a4f1d44b271a91b
Matthew Herod <matthew.herod@myunidays.com>
2017-09-29 17:58:37 +0100

    Fix waitWhileUiNodeExists requiring at least one node

--- adb-kotlin/src/main/kotlin/co/herod/adbwrapper/testing/waitWhileUiNodeExists.kt
+++ adb-kotlin/src/main/kotlin/co/herod/adbwrapper/testing/waitWhileUiNodeExists.kt
@@ -2,11 +2,13 @@ package co.herod.adbwrapper.testing
 
 import co.herod.adbwrapper.model.UiNode
 import co.herod.adbwrapper.ui.sourceUiNodes
+import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
 fun AdbDeviceTestHelper.waitWhileUiNodeExists(predicate: (UiNode) -> Boolean?) = with(adbDevice) {
 
-    sourceUiNodes()
+    Observable.timer(100, TimeUnit.MILLISECONDS)
+            .flatMap { sourceUiNodes() }
             .filter { predicate(it) == true && it.visible }
             .doOnNext { println("Blocking while visible: $it") }
             .buffer(200, TimeUnit.MILLISECONDS)
@@ -15,7 +17,6 @@ fun AdbDeviceTestHelper.waitWhileUiNodeExists(predicate: (UiNode) -> Boolean?) =
                     println("Blocking on ${list.size} uiNodes")
                 }
             }
-            .takeWhile { it.isNotEmpty() }
             .timeout(30, TimeUnit.SECONDS)
             .blockingSubscribe()
 }
