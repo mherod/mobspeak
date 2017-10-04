ad4a90f3acf39a67386524ff1b952287491737a2
Matthew Herod <matthew.herod@myunidays.com>
2017-10-04 11:46:35 +0100

    Extra bits for uiautomator rpc bridge

--- uiautomator/start-server.sh
+++ uiautomator/start-server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-adb install -r app-debug-androidTest.apk || adb uninstall com.github.uiautomator; adb install -r app-debug-androidTest.apk
-adb shell am instrument -w -e package com.github.uiautomator com.github.uiautomator.test/android.support.test.runner.AndroidJUnitRunner &
+adb install -r app-debug.apk || ( adb uninstall com.github.uiautomator; adb install -r app-debug.apk )
+adb install -r app-debug-androidTest.apk || ( adb uninstall com.github.uiautomator.test; adb install -r app-debug-androidTest.apk )
+
 adb forward tcp:9008 tcp:9008
+adb shell am instrument -w -e package com.github.uiautomator com.github.uiautomator.test/android.support.test.runner.AndroidJUnitRunner
