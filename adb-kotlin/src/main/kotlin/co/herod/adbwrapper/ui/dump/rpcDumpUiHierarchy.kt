df16b68e4fa0b69fef3f52f4d16b8e6f117abd79
Matthew Herod <matthew.herod@myunidays.com>
2017-10-04 11:46:35 +0100

    Extra bits for uiautomator rpc bridge

--- adb-kotlin/src/main/kotlin/co/herod/adbwrapper/ui/dump/rpcDumpUiHierarchy.kt
+++ adb-kotlin/src/main/kotlin/co/herod/adbwrapper/ui/dump/rpcDumpUiHierarchy.kt
@@ -4,6 +4,9 @@ import co.herod.adbwrapper.uiautomator.RpcBlah
 import io.reactivex.Observable
 
 fun rpcDumpUiHierarchy(): Observable<String> {
+
+    // if not started rpc then error
+
 //    println("rpcDumpUiHierarchy")
     return RpcBlah.tryRpcUi().toObservable() // .doOnNext { println(it) }
 }
--- adb-kotlin/src/main/kotlin/co/herod/adbwrapper/ui/streamUi.kt
+++ adb-kotlin/src/main/kotlin/co/herod/adbwrapper/ui/streamUi.kt
@@ -1,26 +1,38 @@
 package co.herod.adbwrapper.ui
 
 import co.herod.adbwrapper.AdbBusManager
+import co.herod.adbwrapper.command
 import co.herod.adbwrapper.model.AdbDevice
 import co.herod.adbwrapper.model.UiNode
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
-fun AdbDevice.streamUi(): Observable<UiNode> =
-        Observable.timer(100, TimeUnit.MILLISECONDS)
-                .observeOn(Schedulers.newThread())
-                .subscribeOn(Schedulers.newThread())
-                .flatMap { dumpUiHierarchy() }
-                .repeat()
-                .doOnEach(AdbBusManager._uiHierarchyBus)
-                .flatMapIterable { it.uiNodes }
-                .doOnEach(AdbBusManager._uiNodeBus)
-                .doOnSubscribe {
-                    println("Subscribe of streamUi")
-                    AdbBusManager.uiHierarchyBusActive = true
-                }
-                .doOnDispose {
-                    println("Dispose of streamUi")
-                    AdbBusManager.uiHierarchyBusActive = false
-                }
+fun AdbDevice.streamUi(): Observable<UiNode> {
+
+    command("shell " +
+            "am " +
+            "instrument " +
+            "-w " +
+            "-e package com.github.uiautomator " +
+            "com.github.uiautomator.test/android.support.test.runner.AndroidJUnitRunner")
+            .observeOn(Schedulers.newThread())
+            .subscribe()
+
+    return Observable.timer(100, TimeUnit.MILLISECONDS)
+            .observeOn(Schedulers.newThread())
+            .subscribeOn(Schedulers.newThread())
+            .flatMap { dumpUiHierarchy() }
+            .repeat()
+            .doOnEach(AdbBusManager._uiHierarchyBus)
+            .flatMapIterable { it.uiNodes }
+            .doOnEach(AdbBusManager._uiNodeBus)
+            .doOnSubscribe {
+                println("Subscribe of streamUi")
+                AdbBusManager.uiHierarchyBusActive = true
+            }
+            .doOnDispose {
+                println("Dispose of streamUi")
+                AdbBusManager.uiHierarchyBusActive = false
+            }
+}
