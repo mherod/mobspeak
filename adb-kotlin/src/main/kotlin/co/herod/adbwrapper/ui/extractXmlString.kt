9650f308ac567da5e74717676f52dbdfcf2ba733
Matthew Herod <matthew.herod@myunidays.com>
2017-10-06 21:09:01 +0100

    More ting

---
+++ adb-kotlin/src/main/kotlin/co/herod/adbwrapper/ui/extractXmlString.kt
@@ -0,0 +1,9 @@
+package co.herod.adbwrapper.ui
+
+fun extractXmlString(it: String) = it
+        .substringAfter('<')
+        .substringBeforeLast('>')
+        .let {
+            when { it.endsWith('>') -> it; else -> "$it>"
+            }
+        }
