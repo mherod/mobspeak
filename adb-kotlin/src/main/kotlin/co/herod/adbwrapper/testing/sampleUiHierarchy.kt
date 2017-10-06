0fbbc021c43e42b61337da95b5e6953e5de8e1f9
Matthew Herod <matthew.herod@myunidays.com>
2017-10-06 18:19:29 +0100

    Fixes

--- adb-kotlin/src/main/kotlin/co/herod/adbwrapper/testing/sampleUiHierarchy.kt
+++ adb-kotlin/src/main/kotlin/co/herod/adbwrapper/testing/sampleUiHierarchy.kt
@@ -1,12 +1,13 @@
 package co.herod.adbwrapper.testing
 
+import co.herod.adbwrapper.model.AdbDevice
 import co.herod.adbwrapper.model.UiHierarchy
 import co.herod.adbwrapper.ui.sourceUiHierarchy
 import co.herod.kotlin.now
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
-fun AdbDeviceTestHelper.sampleUiHierarchy(): Observable<UiHierarchy> =
+fun AdbDevice.sampleUiHierarchy(): Observable<UiHierarchy> =
         Observable.just(this)
                 .map { now() }
                 .flatMap { startTime ->
--- adb-kotlin/src/main/kotlin/co/herod/adbwrapper/ui/sourceUiHierarchy.kt
+++ adb-kotlin/src/main/kotlin/co/herod/adbwrapper/ui/sourceUiHierarchy.kt
@@ -2,22 +2,12 @@
 
 package co.herod.adbwrapper.ui
 
-import co.herod.adbwrapper.AdbBusManager
+import co.herod.adbwrapper.model.AdbDevice
 import co.herod.adbwrapper.model.UiHierarchy
-import co.herod.adbwrapper.testing.AdbDeviceTestHelper
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 
-fun AdbDeviceTestHelper.sourceUiHierarchy(): Observable<UiHierarchy> = with(adbDevice) {
-
-    Observable.just(this)
-            .flatMap {
-                if (AdbBusManager.uiHierarchyBusActive) {
-                    AdbBusManager.uiHierarchyBus
-                } else {
-                    subscribeUiHierarchySource()
-                }
-            }
-            .observeOn(Schedulers.newThread())
-            .subscribeOn(Schedulers.computation())
-}
+fun AdbDevice.sourceUiHierarchy(): Observable<UiHierarchy> = Observable.just(this)
+        .flatMap { it.myUiHierarchyBus }
+        .observeOn(Schedulers.newThread())
+        .subscribeOn(Schedulers.computation())
